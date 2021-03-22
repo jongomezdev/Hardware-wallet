776d6c6c9816890d3cb29358ea2b2e31f7edbac6
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-22 14:27:41 -0400

    rearranging files

---
+++ client/webpack.config.js
@@ -0,0 +1,22 @@
+const config = {
+  entry: './assets/js/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
