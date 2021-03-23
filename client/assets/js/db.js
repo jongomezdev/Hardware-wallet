1e6baf73e6c031e7e361b6f8c0661777c6ccac1d
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-22 21:11:34 -0400

    create saveRecord function

--- client/assets/js/db.js
+++ client/assets/js/db.js
@@ -1,1 +1,16 @@
 let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
