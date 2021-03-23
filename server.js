0f34970977bccc32353c1ba78cb725d917fcf1c0
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-23 14:46:40 -0400

    redeploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
+//test
 
 const PORT = process.env.PORT || 3000;
 
