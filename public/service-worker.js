297713834a9c17b68b3caa9ea8a143f16a446691
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-23 11:45:23 -0400

    remove exports

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
 self.addEventListener('install', function (evt) {
