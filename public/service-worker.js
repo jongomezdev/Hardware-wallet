3136aee43afb1c9c71e1788273a117a756f3d230
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-23 14:43:53 -0400

    add logic to serviceworker for offline access

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,17 +2,19 @@ const FILES_TO_CACHE = [
   '/',
   'manifest.webmanifest',
   'index.html',
-  '/assets/css/styles.css',
+  'assets/css/styles.css',
   'assets/js/index.js',
-  'assets/js/db.js',
-  'assets/icons/icon-192x192.png',
-  'assets/icons/icon-512x512.png',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+  'assets/js/db.js',
+  'assets/images/icons/icon-192x192.png',
+  'assets/images/icons/icon-512x512.png',
 ];
 
-const CACHE_NAME = 'static-cache-v1';
+const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 
+// TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser
+// install
 self.addEventListener('install', function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -41,6 +43,7 @@ self.addEventListener('activate', function (evt) {
   self.clients.claim();
 });
 
+// fetch
 self.addEventListener('fetch', function (evt) {
   // cache successful requests to the API
   if (evt.request.url.includes('/api/')) {
@@ -68,6 +71,7 @@ self.addEventListener('fetch', function (evt) {
     return;
   }
 
+  // if the request is not for the API, serve static assets using "offline-first" approach.
   evt.respondWith(
     caches.match(evt.request).then(function (response) {
       return response || fetch(evt.request);
