d49dcc81f09697adbf3937c0f49ce3000fbdd2d0
Jonathan Gomez <jongomezdev@gmail.com>
2021-03-22 21:18:43 -0400

    finish logic for storing data while offline

--- client/assets/js/db.js
+++ client/assets/js/db.js
@@ -14,3 +14,46 @@ request.onsuccess = function (event) {
     checkDatabase();
   }
 };
+
+request.onerror = function (event) {
+  console.log(`Something went wrong... ${event.target.errorCode}`);
+};
+
+export function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+
+  const store = transaction.objectStore('pending');
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(['pending'], 'readwrite');
+
+  const store = transaction.objectStore('pending');
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((res) => res.json())
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite');
+
+          const store = transaction.objectStore('pending');
+
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener('online', checkDatabase);
